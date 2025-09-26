2708d8e5071753aaa0c66e8c5d1ead2680fbb6fa
leech <chamhaolee@gmail.com>
2025-09-26 18:20:09 +1200

    Initial commit

---
+++ allegro_hand/source/allegro_hand/allegro_hand/tasks/direct/allegro_hand/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+
+gym.register(
+    id="Template-Allegro-Hand-Direct-v0",
+    entry_point=f"{__name__}.allegro_hand_env:AllegroHandEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.allegro_hand_env_cfg:AllegroHandEnvCfg",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+    },
+)
